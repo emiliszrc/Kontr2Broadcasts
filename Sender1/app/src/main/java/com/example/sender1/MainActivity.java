4a6bfce5b773500b2aec36c1d38ccfd6752055e2
emzar <em.zareckis@gmail.com>
2020-11-26 16:32:56 +0200

    Styling and notification

--- Sender1/app/src/main/java/com/example/sender1/MainActivity.java
+++ Sender1/app/src/main/java/com/example/sender1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.sender1;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ins = this;
+    }
+
+    private static MainActivity ins;
+
+    public static MainActivity getInstance() {
+        return ins;
     }
 
     public void sendBroadcast(View v) {
@@ -34,5 +48,39 @@ public class MainActivity extends AppCompatActivity {
         ApplicationInfo applicationInfo = context.getApplicationInfo();
         int stringId = applicationInfo.labelRes;
         return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
+    }
+
+    public void createNotification(String action)
+    {
+        int NOTIFICATION_ID = 234;
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "my_channel_01";
+            CharSequence name = "my_channel";
+            String Description = "This is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_channel_01")
+                .setSmallIcon(R.drawable.ic_stat_accessibility)
+                .setContentTitle("Broadcast received")
+                .setContentText("2 programa priėmė transliaciją su raktu: "+action);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
--- Sender1/app/src/main/java/com/example/sender1/ResponseBroadcastReceiver.java
+++ Sender1/app/src/main/java/com/example/sender1/ResponseBroadcastReceiver.java
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
+import androidx.core.app.NotificationCompat;
 
 public class ResponseBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "Response broadcast triggered with action"+intent.getAction(), Toast.LENGTH_SHORT).show();
+            MainActivity.getInstance().createNotification(intent.getAction());
     }
+
+
 }
